8ad9bac4d43e5f0c9ed0558c07011ec6aa5b7686
JCernei <jonnycernei@gmail.com>
2021-12-03 14:39:26 +0200

    BT-14 Fetch connections/accounts/transactions

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,10 +5,61 @@ require 'json'
 class AccountController < ApplicationController
   before_action :authenticate_user!
   def index
+    uri = URI("https://www.saltedge.com/api/v5/connections?customer_id=#{current_user.customer_id}")
+    req = Net::HTTP::Get.new(uri)
+    req['Accept'] = 'application/json'
+    req['Content-type'] = 'application/json'
+    req['App-id'] = ENV.fetch('APP_ID')
+    req['Secret'] = ENV.fetch('SECRET')
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+    
+    if res.code == '200'
+      @connections = JSON.parse(res.body)['data']
+      @accounts = fetch_accounts(@connections)
+    end
   end
- 
+
+  def fetch_accounts(connections)
+    accounts = []
+    for connection in connections do
+      uri = URI("https://www.saltedge.com/api/v5/accounts?connection_id=#{connection["id"]}")
+      req = Net::HTTP::Get.new(uri)
+      req['Accept'] = 'application/json'
+      req['Content-type'] = 'application/json'
+      req['App-id'] = ENV.fetch('APP_ID')
+      req['Secret'] = ENV.fetch('SECRET')
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+      if res.code == '200'
+        accounts += JSON.parse(res.body)['data']
+      end
+    end
+    return accounts
+  end
+
+  def remove_connection
+      uri = URI("https://www.saltedge.com/api/v5/connections/#{params['id']}")
+      req = Net::HTTP::Delete.new(uri)
+      req['Accept'] = 'application/json'
+      req['Content-type'] = 'application/json'
+      req['App-id'] = ENV.fetch('APP_ID')
+      req['Secret'] = ENV.fetch('SECRET')
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+      if res.code == '200'
+        redirect_to :action => 'index'
+      end
+  end
+
+  def destroy
+    redirect_to :action => 'index'
+  end
+
   def create_session
-    puts Time.now.utc.to_date.to_s
     uri = URI('https://www.saltedge.com/api/v5/connect_sessions/create')
     req = Net::HTTP::Post.new(uri)
     req['Accept'] = 'application/json'
@@ -31,13 +82,9 @@ class AccountController < ApplicationController
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(req)
     end
-    puts JSON.parse(req.body)
-    puts
-    puts JSON.parse(res.body)
-
+    
     if res.code == '200'
       redirect_to JSON.parse(res.body)['data']['connect_url']
     end
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'account/destroy'
+  delete 'account/remove_connection'
+  get 'transactions/index'
   get "account/index"
   post "account/create_session"
   devise_for :users, controllers: {
